910c86aaf58b5b88e0279b8dd588cd09e1e80d6d
Meoyoug <eodyd426@gmail.com>
2024-05-08 01:50:30 +0900

    Feat : 채팅앱에 필요한 util 메서드 추가
    - check_entered_chatroom : 유저가 채팅방에 참여한 유저인지 확인하는 메서드
    - change_entered_status : 요청을 보낸 유저의 상태 변환하는 메서드
    - delete_chatroom : 둘다 채팅방을 나가면 채팅방을 삭제해주는 메서드

---
+++ apps/chat/utils.py
@@ -0,0 +1,38 @@
+from apps.account.models import Account
+from apps.chat.models import Chatroom
+
+
+def check_entered_chatroom(chatroom: Chatroom, user: Account) -> bool:
+    """
+    만약 유저가 borrower이면 borrower_status의 값을 반환해서 채팅방에 존재하는지 나갔는지를 판단
+    만약 유저가 lender이면 lender_status의 값을 반환해서 채팅방에 존재하는지 나갔는지를 판단
+    유저가 둘다 속하지 않는다면 잘못된 접근으로 판단하고 False를 반환
+    """
+    if chatroom.borrower == user:
+        return chatroom.borrower_status
+    elif chatroom.lender == user:
+        return chatroom.lender_status
+    else:
+        return False
+
+
+def change_entered_status(chatroom: Chatroom, user: Account) -> None:
+    """
+    채팅방에 참여해 있는 유저의 역할을 파악하고, 그에 맞는 status를 False로 변경(False는 나가기한 상태)
+    """
+    if chatroom.borrower == user:
+        chatroom.borrower_status = False
+    if chatroom.lender == user:
+        chatroom.lender_status = False
+    chatroom.save()
+
+
+def delete_chatroom(chatroom: Chatroom) -> bool:
+    """
+    채팅방에 참여한 둘 중 한명이라도 나간 상태라면,
+    해당 유저가 나가기를 눌렀을 때 채팅방에 남아있는 유저가 없기 때문에 채팅방을 삭제함.
+    """
+    if not chatroom.borrower_status or not chatroom.lender_status:
+        chatroom.delete()
+        return True
+    return False
