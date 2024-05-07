ca946aa68bb3980f4d913d4847e9722e25ce66d9
Meoyoug <eodyd426@gmail.com>
2024-05-08 01:48:47 +0900

    Feat : chat 앱의 urlpatterns 를 추가
    - urlpatterns에 path("api/chat/") 추가

--- config/urls.py
+++ config/urls.py
@@ -31,6 +31,7 @@ urlpatterns = [
     path("api/schema/swagger-ui/", SpectacularSwaggerView.as_view(url_name="schema"), name="swagger-ui"),
     path("api/schema/redoc/", SpectacularRedocView.as_view(url_name="schema"), name="redoc"),
     path("api/categories/", include("apps.category.urls")),
+    path("api/chat/", include("apps.chat.urls")),
 ]
 
 if settings.DEBUG:
