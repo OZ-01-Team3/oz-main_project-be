4701b24cc104c477359be4505d2f60d936f2bac9
Meoyoug <eodyd426@gmail.com>
2024-05-08 01:47:44 +0900

    Feat : ChatRoomView, ChatDetailView 를 위한 엔드포인트 추가
    - api/chat/ : ChatRoomView, 채팅방 개설
    - api/chat/<int:chatroom_id>/ : ChatDetailView, 채팅방 메시지 불러오기, 채팅방 나가기 및 삭제

---
+++ apps/chat/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from apps.chat import views
+
+
+urlpatterns = [
+    path('', views.ChatRoomView.as_view(), name='chatroom'),
+    path('<int:chatroom_id>/', views.ChatDetailView.as_view(), name='chat-detail')
+]
