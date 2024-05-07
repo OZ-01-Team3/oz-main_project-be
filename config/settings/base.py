56feaaafbcea0b1d15b08c69ba5bde311692f76f
yyysolhhh <solbaram37@gmail.com>
2024-05-07 16:55:54 +0900

    Test: category 테스트 코드 추가
    CategoryModelTest
    - 모든 카테고리 조회 테스트 코드 작성
    
    base.py
    - drf_spectacular 설정 오류 수정

--- config/settings/base.py
+++ config/settings/base.py
@@ -137,7 +137,7 @@ DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 AUTH_USER_MODEL = "account.Account"
 
 REST_FRAMEWORK = {
-    "DEFAULT_SCHEMA_CLASSES": "drf_spectacular.openapi.AutoSchema",
+    "DEFAULT_SCHEMA_CLASS": "drf_spectacular.openapi.AutoSchema",
     "DEFAULT_PERMISSION_CLASSES": ("rest_framework.permissions.IsAuthenticated",),
 }
 
