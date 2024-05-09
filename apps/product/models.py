32957d54af60e3b9a89401232d7c47268eb9204a
friedrice93 <153698085+friedrice93@users.noreply.github.com>
2024-05-09 15:56:12 +0900

    Feat: product 모델 필드 수정
    Product
    - size_choice 제거
    - brand 필드 주석 처리

--- apps/product/models.py
+++ apps/product/models.py
@@ -5,23 +5,13 @@ from apps.common.utils import uuid4_generator
 
 
 class Product(models.Model):
-    SIZE_CHOICE = [
-        ('XS', 'XSmall'),
-        ('S', 'Small'),
-        ('M', 'Medium'),
-        ('L', 'Large'),
-        ('XL', 'XLarge'),
-        ('XXL', 'XXLarge'),
-        ('XXXL', 'XXXLarge'),
-        ('FREE', 'FreeSize'),
-    ]
     uuid = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     name = models.CharField(max_length=50)
-    brand = models.ForeignKey(on_delete=models.SET_NULL, null=True)  # 브랜드
+    #brand = models.ForeignKey(on_delete=models.SET_NULL, null=True)  # 브랜드
     condition = models.TextField()  # 옷 상태
     purchasing_price = models.IntegerField()  # 구매 당시 가격
     rental_fee = models.IntegerField()  # 대여 비용
-    size = models.CharField(max_length=10, choices=SIZE_CHOICE)  # 사이즈
+    size = models.CharField(max_length=10)  # 사이즈
     views = models.IntegerField(default=0)  # 조회수
     #product_category_id = models.ForeignKey()
     #style_category_id = models.ManyToManyField()
