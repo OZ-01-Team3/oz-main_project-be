a1111cd24888df3e6f1db6f14669a6e5896f916d
yyysolhhh <solbaram37@gmail.com>
2024-05-12 12:18:55 +0900

    Feat: AUTH_PASSWORD_VALIDATORS 수정
    base.py
    - 기본 설정인 AUTH_PASSWORD_VALIDATORS 4가지 중에
      UserAttributeSimilarityValidator랑 CommonPasswordValidator는 사용하지
      않기로 함

--- config/settings/base.py
+++ config/settings/base.py
@@ -120,15 +120,15 @@ ASGI_APPLICATION = "config.asgi.application"
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
 
 AUTH_PASSWORD_VALIDATORS = [
-    {
-        "NAME": "django.contrib.auth.password_validation.UserAttributeSimilarityValidator",
-    },
+    # {
+    #     "NAME": "django.contrib.auth.password_validation.UserAttributeSimilarityValidator",
+    # },
     {
         "NAME": "django.contrib.auth.password_validation.MinimumLengthValidator",
     },
-    {
-        "NAME": "django.contrib.auth.password_validation.CommonPasswordValidator",
-    },
+    # {
+    #     "NAME": "django.contrib.auth.password_validation.CommonPasswordValidator",
+    # },
     {
         "NAME": "django.contrib.auth.password_validation.NumericPasswordValidator",
     },
