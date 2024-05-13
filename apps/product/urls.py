d6c65112c5dd58c04d0d884c29876d185da1562b
friedrice93 <153698085+friedrice93@users.noreply.github.com>
2024-05-13 17:32:50 +0900

    Style: views.py, urls.py 주석 제거

--- apps/product/urls.py
+++ apps/product/urls.py
@@ -10,26 +10,3 @@ urlpatterns = [
     path("", include(router.urls)),
     # path('api-auth/', include('rest_framework.urls', namespace='rest_framework'))
 ]
-
-# from django.urls import path
-#
-# from apps.product.views import (  # ProductStatusUpdateAPIView
-#     ProductCreateAPIView,
-#     ProductDeleteAPIView,
-#     ProductDetailAPIView,
-#     ProductListAPIView,
-#     ProductPartialUpdateAPIView,
-#     ProductSearchAPIView,
-#     ProductUpdateAPIView,
-# )
-#
-# urlpatterns = [
-#     path("", ProductListAPIView.as_view(), name="product-list"),
-#     path("", ProductCreateAPIView.as_view(), name="product-create"),
-#     path("<int:product_id>/", ProductDetailAPIView.as_view(), name="product-detail"),
-#     path("<int:product_id>/", ProductUpdateAPIView.as_view(), name="product-update"),
-#     path("<int:product_id>/",ProductPartialUpdateAPIView.as_view(),name="product-partial-update",),
-#     path("<int:product_id>/", ProductDeleteAPIView.as_view(), name="product-delete"),
-#     path("", ProductSearchAPIView.as_view(), name="product-search"),
-#     # path("api/products/, ProductStatusUpdateAPIView.as_view(), name='product-status-update'),
-# ]
--- config/settings/base.py
+++ config/settings/base.py
@@ -168,7 +168,7 @@ REST_FRAMEWORK = {
     "DEFAULT_SCHEMA_CLASS": "drf_spectacular.openapi.AutoSchema",
     "DEFAULT_PERMISSION_CLASSES": ("rest_framework.permissions.IsAuthenticated",),
     "DEFAULT_AUTHENTICATION_CLASSES": (
-        "rest_framework_simplejwt.authentication.JWTAuthentication",
+        #"rest_framework_simplejwt.authentication.JWTAuthentication",
         "rest_framework.authentication.SessionAuthentication",
         "dj_rest_auth.jwt_auth.JWTCookieAuthentication",
     ),
