fb4ab8013a4ac4db8f0841ff87ff12f38bd8ac62
yyysolhhh <solbaram37@gmail.com>
2024-05-14 02:42:50 +0900

    Style: mypy 타입 에러 고침
    - 모델 명시해서 해결

--- apps/user/api_schema.py
+++ apps/user/api_schema.py
@@ -1,34 +1,36 @@
 from rest_framework import serializers
 
+from apps.user.models import Account
 
-class LoginRequestSchema(serializers.Serializer):
+
+class LoginRequestSchema(serializers.Serializer[Account]):
     email = serializers.EmailField()
     password = serializers.CharField()
 
 
-class LoginResponseSchema(serializers.Serializer):
+class LoginResponseSchema(serializers.Serializer[Account]):
     access = serializers.CharField()
     refresh = serializers.CharField()
 
 
-class ConfirmRequestSchema(serializers.Serializer):
+class ConfirmRequestSchema(serializers.Serializer[Account]):
     email = serializers.EmailField()
     code = serializers.CharField()
 
 
-class ConfirmResponseSchema(serializers.Serializer):
+class ConfirmResponseSchema(serializers.Serializer[Account]):
     message = serializers.CharField()
 
 
-class SendRequestSchema(serializers.Serializer):
+class SendRequestSchema(serializers.Serializer[Account]):
     email = serializers.EmailField()
 
 
-class SendResponseSchema(serializers.Serializer):
+class SendResponseSchema(serializers.Serializer[Account]):
     message = serializers.CharField()
 
 
-class SignupRequestSchema(serializers.Serializer):
+class SignupRequestSchema(serializers.Serializer[Account]):
     email = serializers.EmailField()
     password1 = serializers.CharField()
     password2 = serializers.CharField()
@@ -36,6 +38,6 @@ class SignupRequestSchema(serializers.Serializer):
     phone = serializers.CharField()
 
 
-class SignupResponseSchema(serializers.Serializer):
+class SignupResponseSchema(serializers.Serializer[Account]):
     access = serializers.CharField()
     refresh = serializers.CharField()
