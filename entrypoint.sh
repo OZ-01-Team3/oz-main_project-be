8f7ebf07851ba0f9bf758ab913a069afeb552196
yyysolhhh <solbaram37@gmail.com>
2024-05-16 01:17:10 +0900

    Chore: 빌드 스크립트 수정
    Caddyfile
    - api, ws 추가
    
    entrypoint.sh
    - DJANGO_SETTINGS_MODULE 설정 추가
    
    create_superuser.py
    - prod, local일때 다른 곳에서 변수 가져옴

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,8 +1,10 @@
 #!/bin/sh
 
 ln -sf prod.py config/settings/settings.py
-sudo apt-get update
-sudo apt-get install vim -y
+#sudo apt-get update
+#sudo apt-get install vim -y
+export DJANGO_SETTINGS_MODULE=config.settings.settings
+
 python manage.py makemigrations
 python manage.py migrate
 python manage.py collectstatic --noinput
@@ -10,4 +12,4 @@ python manage.py shell < tools/create_superuser.py
 #python manage.py runserver 0.0.0.0:80
 
 gunicorn --bind 0:8000 config.wsgi:application
-#uvicorn config.asgi:application
+#uvicorn config.asgi:application --port 80 --workers 4
