aadf44f745506dafd5cbe05b8f4ba902ac414ba7
yyysolhhh <solbaram37@gmail.com>
2024-05-17 11:22:15 +0900

    Feat: 페이지네이션 기능 추가
    base.py
    - DEFAULT_PAGINATION_CLASS에 PageNumberPagination 추가
    - 한 페이지에 24개씩으로 설정

--- config/settings/base.py
+++ config/settings/base.py
@@ -175,6 +175,8 @@ REST_FRAMEWORK = {
         "dj_rest_auth.jwt_auth.JWTCookieAuthentication",
     ),
     "DEFAULT_FILTER_BACKENDS": ("django_filters.rest_framework.DjangoFilterBackend",),
+    "DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.PageNumberPagination",
+    "PAGE_SIZE": 24,
 }
 
 # drf-spectacular 관련 설정
