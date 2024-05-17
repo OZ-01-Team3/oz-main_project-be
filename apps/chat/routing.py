f0dcc38467786a9cafc5f7f329ec9f8942e5e817
Meoyoug <eodyd426@gmail.com>
2024-05-18 02:46:40 +0900

    Chore: url정리

--- apps/chat/routing.py
+++ apps/chat/routing.py
@@ -4,7 +4,4 @@ from . import consumers, views
 
 websocket_urlpatterns = [
     path("ws/chat/<int:chatroom_id>/", consumers.ChatConsumer.as_asgi()),
-    path("", views.ChatRoomView.as_view(), name="chatroom"),
-    path("<int:chatroom_id>/", views.ChatDetailView.as_view(), name="chat-detail"),
-    path("enter/", views.render_chat),
 ]
