6820ffe8cbf99560ed18153b425a6412cd112967
yyysolhhh <solbaram37@gmail.com>
2024-05-17 11:47:52 +0900

    Fix: product permission 리턴 타입 수정
    IsLenderOrReadOnly
    - getattr로 lender 가져옴
    - request.user str로 변환하지 않아야 타입이 맞음

--- apps/product/permissions.py
+++ apps/product/permissions.py
@@ -9,5 +9,5 @@ class IsLenderOrReadOnly(permissions.BasePermission):
     def has_object_permission(self, request: Request, view: APIView, obj: Any) -> bool:
         if request.method in permissions.SAFE_METHODS:
             return True
-        return str(request.user) == obj.lender.email
-        # return str(request.user) == getattr(obj, "lender.email", None)
+        # return str(request.user) == obj.lender.email
+        return request.user == getattr(obj, "lender", None)
