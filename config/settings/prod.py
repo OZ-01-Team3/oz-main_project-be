53e3ed38bfd87d40d498caf07eaceb1be4a28261
yyysolhhh <solbaram37@gmail.com>
2024-05-20 14:46:00 +0900

    Chore: docker-cicd.yml 추가
    - github action yml 파일 작성
    - docker build, deploy 과정
    - access token 기간 늘림

--- config/settings/prod.py
+++ config/settings/prod.py
@@ -50,7 +50,7 @@ CHANNEL_LAYERS = {
 
 # djangorestframework-simplejwt 관련 설정
 SIMPLE_JWT = {
-    "ACCESS_TOKEN_LIFETIME": timedelta(hours=1),  # default: minutes=5
+    "ACCESS_TOKEN_LIFETIME": timedelta(days=1),  # default: minutes=5
     "REFRESH_TOKEN_LIFETIME": timedelta(days=7),  # default: days=1
     "ROTATE_REFRESH_TOKENS": False,
     "BLACKLIST_AFTER_ROTATION": False,
