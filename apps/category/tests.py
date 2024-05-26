7e4a68bb4f7a6b42819c688d55f658cfbd5aa33e
yyysolhhh <solbaram37@gmail.com>
2024-05-26 22:43:06 +0900

    Test: category, mypage 테스트 코드 추가
    StyleListTest
    - get_all_styles 테스트 추가
    
    MyProductTest
    - get_my_products 테스트 추가

--- apps/category/tests.py
+++ apps/category/tests.py
@@ -3,10 +3,10 @@ from django.urls import reverse
 from rest_framework import status
 from rest_framework.test import APIClient
 
-from apps.category.models import Category
+from apps.category.models import Category, Style
 
 
-class CategoryModelTest(TestCase):
+class CategoryListTest(TestCase):
     def setUp(self) -> None:
         Category.objects.create(name="test_category1")
         Category.objects.create(name="test_category2")
@@ -20,4 +20,17 @@ class CategoryModelTest(TestCase):
         response = client.get(url)
         self.assertEqual(response.status_code, status.HTTP_200_OK)
         # self.assertEqual(response.data.get("count"), 2)
+        self.assertEqual(len(response.data), 2)
+
+
+class StyleListTest(TestCase):
+    def setUp(self) -> None:
+        Style.objects.create(name="test_style1")
+        Style.objects.create(name="test_style2")
+
+    def test_get_all_styles(self) -> None:
+        client = APIClient()
+        url = reverse("style-list")
+        response = client.get(url)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
         self.assertEqual(len(response.data), 2)
