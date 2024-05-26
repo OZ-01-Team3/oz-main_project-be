44415b9e1bf9f62d58c743d4f36b9745ffe7321c
yyysolhhh <solbaram37@gmail.com>
2024-05-26 21:17:16 +0900

    Feat: MyProductSerializer 필드 수정
    - lender 제거
    - url, images 추가

--- apps/mypage/serializers.py
+++ apps/mypage/serializers.py
@@ -1,9 +1,32 @@
 from rest_framework import serializers
 
 from apps.product.models import Product
+from apps.product.serializers import ProductImageSerializer
 
 
 class MyProductSerializer(serializers.ModelSerializer):
+    images = ProductImageSerializer(many=True, read_only=True)
+
     class Meta:
         model = Product
-        fields = "__all__"
+        fields = (
+            "url",
+            "uuid",
+            "name",
+            "brand",
+            "condition",
+            "description",
+            "purchase_date",
+            "purchase_price",
+            "rental_fee",
+            "size",
+            "views",
+            "product_category",
+            "styles",
+            "status",
+            "amount",
+            "region",
+            "created_at",
+            "updated_at",
+            "images",
+        )
