680bfe38cfa96e7aab25a339afd4f0e7ed710477
Meoyoug <eodyd426@gmail.com>
2024-05-29 21:00:44 +0900

    Feat: RentalHistoryBorrowerView, RentalHistoryLenderView, RentalHistoryUpdateView 추가
    - RentalHistoryBorrowerView: 대여 신청 및 대여 내역 조회, 대여신청 중복시 400
    - RentalHistoryLenderView: 판매 내역 조회
    - RentalHistoryUpdateView: 대여 상태 업데이트 및 반납일 수정

--- apps/product/views.py
+++ apps/product/views.py
@@ -1,15 +1,13 @@
-from typing import Any
 
 from django.db.models import QuerySet
-from django.utils.decorators import method_decorator
-from django.views.decorators.cache import cache_page
 from django_filters.rest_framework import DjangoFilterBackend
-from requests import Response
 from rest_framework import generics, permissions, status, viewsets
 from rest_framework.filters import OrderingFilter, SearchFilter
 from rest_framework.parsers import FormParser, MultiPartParser
 from rest_framework.request import Request
+from rest_framework.response import Response
 from rest_framework.serializers import BaseSerializer
+from rest_framework.generics import ListAPIView, ListCreateAPIView, UpdateAPIView
 
 from apps.product.models import Product, ProductImage, RentalHistory
 from apps.product.permissions import IsLenderOrReadOnly
@@ -38,26 +36,48 @@ class ProductViewSet(viewsets.ModelViewSet[Product]):
         return Product.objects.all().order_by("-created_at")
 
 
-class RentalHistoryViewSet(viewsets.ModelViewSet[RentalHistory]):
-    queryset = RentalHistory.objects.all()
+class RentalHistoryBorrowerView(ListCreateAPIView[RentalHistory]):
     serializer_class = RentalHistorySerializer
     permission_classes = [permissions.IsAuthenticated]
-    filter_backends = [SearchFilter, OrderingFilter]
-    search_fields = ["product__name", "lender"]
-    ordering_fields = ["rental_date", "return_date"]
 
     def get_queryset(self) -> QuerySet[RentalHistory]:
-        # 반납 상태가 아직 False인 대여 기록만 반환하도록 필터링
-        queryset = super().get_queryset()
-        return queryset.filter(returned=False)
+        return RentalHistory.objects.filter(borrower=self.request.user)
 
-    # 대여 상태 변경을 위한 엔드포인트 추가
-    # def return_rental(self, request, *args, **kwargs):
-    #     rental = self.get_object()
-    #     rental.returned = True
-    #     rental.save()
-    #     return Response({"message": "반납이 완료 되었습니다."}, status=status.HTTP_200_OK)
-    #
-    # # 대여 상태 변경을 위한 라우트 추가
-    # def return_rental(self, request, *args, **kwargs):
-    #     return self.return_rental_record(request, *args, **kwargs)
+    def list(self, request, *args, **kwargs):
+        queryset = self.filter_queryset(self.get_queryset())
+
+        serializer = self.get_serializer(queryset, many=True)
+        return Response(serializer.data)
+
+    def create(self, request, *args, **kwargs):
+        data = request.data.copy()
+        data["borrower_id"] = self.request.user.id
+        serializer = self.get_serializer(data=data)
+        serializer.is_valid(raise_exception=True)
+        self.perform_create(serializer)
+        headers = self.get_success_headers(serializer.data)
+        return Response(serializer.data, status=status.HTTP_201_CREATED, headers=headers)
+
+
+class RentalHistoryLenderView(ListAPIView[RentalHistory]):
+    serializer_class = RentalHistorySerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+    def get_queryset(self) -> QuerySet[RentalHistory]:
+        queryset = RentalHistory.objects.filter(product__lender=self.request.user)
+        return queryset
+
+    def list(self, request, *args, **kwargs):
+        queryset = self.filter_queryset(self.get_queryset())
+
+        serializer = self.get_serializer(queryset, many=True)
+        return Response(serializer.data)
+
+
+class RentalHistoryUpdateView(UpdateAPIView[RentalHistory]):
+    serializer_class = RentalHistorySerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+    def get_queryset(self) -> QuerySet[RentalHistory]:
+        queryset = RentalHistory.objects.filter(borrower=self.request.user)
+        return queryset
